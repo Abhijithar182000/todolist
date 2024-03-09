74d731ce7bcecf4fe4c5c6bdb543416c3675c45e
Abhi <abhijithar18200@gmail.com>
2024-03-09 17:01:56 +0630

    created main.py file

---
+++ main.py
@@ -0,0 +1,26 @@
+#TO DO LIST
+
+while True:
+ print("##################")
+ print("TO DO LIST APP ")
+ print("##################")
+ print("1 - Add to list" )
+ print("2 - Display  list" )
+ print("3 - Remove from  list" )
+ print("4 - Exist  list" )
+
+ option = input ("select your option.")
+
+ if option == '1':
+    print("Add")
+ elif option == '2':
+    print("Display")
+ elif option == '3':
+    print("Remove")
+ elif option == '4':
+    print("Exist")
+    break
+ else:
+    print("invalid option")                
+
+
